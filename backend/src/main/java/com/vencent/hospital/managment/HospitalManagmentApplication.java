b0f1797e0402ee70ceaebc32eaff195b96f99fe6
Dhanashreebhite <dhanashree6nov@gmail.com>
2020-11-18 23:01:45 +0530

    Backend code is added.

---
+++ backend/src/main/java/com/vencent/hospital/managment/HospitalManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.vencent.hospital.managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagmentApplication.class, args);
+	}
+
+}
